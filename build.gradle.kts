3acf9628ab9685c3d0c13c00aec221d66c3d4391
moomani <moonman@kakao.com>
2024-08-13 03:50:45 +0900

    add logging dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("io.github.oshai:kotlin-logging-jvm:5.1.1")
 	implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 }
---
+++ src/main/kotlin/com/ajouin/notice_view/Logger.kt
@@ -0,0 +1,5 @@
+package com.ajouin.notice_view
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+
+val logger = KotlinLogging.logger() {}
